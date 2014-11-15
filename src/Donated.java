6e4aab64dc8a45724e2301decb8a63a91c18fa8b
Wheeler <wlaw411@gmail.com>
2014-11-14 23:47:56 -0500

    Forgot to include renamed class file.

---
+++ src/Donated.java
@@ -0,0 +1,151 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+
+/**
+ * This class represents a single row in the donation table. The attributes
+ * represent the columns in the table. 
+ * @author Wheeler
+ *
+ */
+public class Donated{
+	public final int donationId;
+	public final int donorId;
+	public final Date donationDate;
+	
+	//This just determines whether to do an insert or an update. Not an actual column in the table.
+	private boolean newObject;
+	
+	/**
+	 * Create an instance of an object that represents an existing row in the database. 
+	 * This constructor is private because the user will not know what the donorId is until
+	 * the object is in the database. 
+	 * This constructor is only used by the static open() method.
+	 * 
+	 * @param donorId		The id of the donor
+	 * @param name			The name of the donor
+	 * @param spouseName	The name of the spouse of the donor
+	 * @param YOG			The year of graduation of the donor
+	 * @param category		The id of the type of donor (student, alumni, etc.)
+	 * @param address		The id of the address of the donor.
+	 * @param circle		The id of the donor circle the donor belongs to. 
+	 */
+	public Donated(int donationId, int donorId, Date donationDate){
+		this.donationId = donationId;
+		this.donorId = donorId;
+		this.donationDate = donationDate;
+		
+		this.newObject = true;
+	}
+	
+	/**
+	 * Returns the Id of this object/row. Throws an exception if save() has not been called on this object. 
+	 * @return int The id of the object/row. 
+	 * @throws SQLNotSavedException Thrown if the object was not saved. 
+	 */
+	public int id() throws SQLNotSavedException{
+		if(this.newObject){
+			throw new SQLNotSavedException("Donation not saved in database");
+		}else{
+			return this.donationId;
+		}
+	}
+	
+	/**
+	 * Saves the object in the database. Determines whether to use an insert or an update statement. 
+	 * @return int A status value if the save was successful or not. 0 if successful, >=1 otherwise. 
+	 */
+	public int save(){
+		Connection c = BetaUniversity.getConnection();
+		PreparedStatement stmt = null;
+		
+		if(!this.newObject)
+			return 1;
+		
+		try {
+			if(c.isClosed()){
+				return 1;
+			}
+			
+			
+			String sql = "insert into donated(donationId, donorId, donationDate) values(?, ?, ?);";
+			
+			stmt = c.prepareStatement(sql);
+			
+			stmt.setInt(1, this.donationId);
+			stmt.setInt(2, this.donorId);
+			stmt.setDate(3, this.donationDate);
+			
+			stmt.executeUpdate();
+			
+			return 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return 1;	
+	}
+	
+	/**
+	 * Opens an an existing row in the databse and creates an object around it. 
+	 * @param donorId	The id of the donor.
+	 * @return Donor	The donor object, or null if there was an error. 
+	 */
+	public static Donated open(int donationId){
+		Connection c = BetaUniversity.getConnection();
+		PreparedStatement stmt = null;
+		
+		Donated donated = null;
+		
+		try {
+			if(c.isClosed()){
+				return null;
+			}
+			
+			String sql = "select donorId, donationDate from donated where donationId = ?;";
+			stmt = c.prepareStatement(sql);
+			
+			stmt.setInt(1, donationId);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			int donorId = 0;
+			Date donationDate = null;
+			if(rs.next()){
+				donorId = rs.getInt(1);
+				donationDate = rs.getDate(2);
+			}
+			if(donationDate == null)
+				return null;
+			
+			rs.close();
+			stmt.close();
+			
+			donated = new Donated(donationId, donorId, donationDate);
+			donated.newObject = false;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return donated;
+		
+	}
+	
+	/**
+	 * Returns a string representation of the donor. 
+	 * @return String the string representation of the donor. 
+	 */
+	public String toString(){
+		String string = "";
+		
+		string += "Donation ID:\t\t" + this.donationId + "\n";
+		string += "Donor ID:\t\t" + this.donorId + "\n";
+		string += "Donation Date:\t\t" + this.donationDate;
+		
+		return string;
+	}
+	
+}
